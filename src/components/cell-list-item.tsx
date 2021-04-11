50983021ffd49cfb287b4d08cafc1f0cda207014
__sunny <rzone845t@gmail.com>
2021-04-12 00:04:57 +0430

    update codeeditor

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,12 +1,15 @@
+import "./cell-list-item.css";
 import { Cell } from "../state";
 import CodeCell from "./code-cell";
+import ActionBar from "./action-bar";
 interface CellListItemProps {
   cell: Cell;
 }
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   return (
-    <div>
-      <CodeCell />
+    <div className="cell-list-item">
+      <CodeCell cell={cell} />
+      <ActionBar id={cell.id} />
     </div>
   );
 };
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CodeEditor from "./code-editor";
 import PreviewCode from "./PreviewCode";
 import bundle from "../bundler";
 import Resizable from "./resizable";
-const CodeCell = () => {
-  const [input, setInput] = useState("");
+import { Cell } from "../state";
+import { useActions } from "../hooks/use-actions";
+import { clearTimeout } from "timers";
+interface CodeCellProps {
+  cell: Cell;
+}
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState("");
+  const { updateCell } = useActions();
 
-  const onClick = async () => {
-    const outPut = await bundle(input);
-    setCode(outPut);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const outPut = await bundle(cell.content);
+      setCode(outPut);
+    }, 750);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cell.content]);
 
   return (
     <Resizable direction="vertical">
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 1;"
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <PreviewCode code={code} />
